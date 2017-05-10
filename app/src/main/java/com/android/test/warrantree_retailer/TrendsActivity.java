1c25145ea7362186a9d62ab4ca69f31f62c7cdfb
Anuj Gupta <junagupta19@gmail.com>
2017-05-10 19:54:15 +0530

    Changed CRM Activity to Trends Activity

--- app/src/main/java/com/android/test/warrantree_retailer/TrendsActivity.java
+++ app/src/main/java/com/android/test/warrantree_retailer/TrendsActivity.java
@@ -1,18 +1,13 @@
 package com.android.test.warrantree_retailer;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
-public class CRMActivity extends AppCompatActivity {
-
+public class TrendsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crm);
-
+        setContentView(R.layout.activity_trends);
     }
 }
